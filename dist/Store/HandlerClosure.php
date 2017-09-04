789d8415722a706a1619290e0e1515452798708c
Coercive <contact@coercive.fr>
2017-09-04 13:22:21 +0200

    Use SuperClosure\Serializer for serialize closures

--- dist/Store/HandlerClosure.php
+++ dist/Store/HandlerClosure.php
@@ -2,10 +2,9 @@
 namespace Coercive\Shop\Cart\Store;
 
 use Closure;
-use SplFileObject;
 use ReflectionFunction;
-use Exception;
-use InvalidArgumentException;
+use SuperClosure\Serializer;
+use SuperClosure\Analyzer\AstAnalyzer;
 
 /**
  * @see |Coercive\Shop\Cart\Cart
@@ -19,30 +18,21 @@ class HandleClosure {
 	protected $_oReflection = null;
 	
 	/** @var string */
-	protected $_sCode = '';
-	
-	/** @var array */
-	protected $_aUsedVariables = [];
+	protected $_sSerialized = '';
 
 	/**
 	 * HandleClosure constructor.
 	 *
-	 * @param $oFunction
+	 * @param Closure $oFunction
 	 */
-	public function __construct($oFunction) {
-
-		# Secure test
-		if ( ! $oFunction instanceOf Closure) {
-			throw new InvalidArgumentException;
-		}
+	public function __construct(Closure $oFunction) {
 
 		# Store for invoke
 		$this->_oClosure = $oFunction;
 		$this->_oReflection = new ReflectionFunction($oFunction);
 
 		# Parse datas for serialize
-		$this->_sCode = $this->_fetchCode();
-		$this->_aUsedVariables = $this->_fetchUsedVariables();
+		$this->_sSerialized = (new Serializer(new AstAnalyzer))->serialize($oFunction);
 
 	}
 
@@ -50,85 +40,22 @@ class HandleClosure {
 	 * @return mixed
 	 */
 	public function __invoke() {
-		$aArgs = func_get_args();
-		return $this->_oReflection->invokeArgs($aArgs);
-	}
-
-	/**
-	 * @return string
-	 */
-	protected function _fetchCode() {
-
-		// Open file and seek to the first line of the closure
-		$oFile = new SplFileObject($this->_oReflection->getFileName());
-		$oFile->seek($this->_oReflection->getStartLine()-1);
-
-		// Retrieve all of the lines that contain code for the closure
-		$sCode = '';
-		while ($oFile->key() < $this->_oReflection->getEndLine()) {
-			$sCode .= $oFile->current();
-			$oFile->next();
-		}
-
-		// Only keep the code defining that closure
-		$iBegin = strpos($sCode, 'function');
-		$iEnd = strrpos($sCode, '}');
-		return (string) substr($sCode, $iBegin, $iEnd - $iBegin + 1);
-
-	}
-
-	/**
-	 * @return array
-	 */
-	protected function _fetchUsedVariables() {
-
-		// Make sure the use construct is actually used
-		$iUseIndex = stripos($this->_sCode, 'use');
-		if ( ! $iUseIndex) { return []; }
-
-		// Get the names of the variables inside the use statement
-		$iBegin = strpos($this->_sCode, '(', $iUseIndex) + 1;
-		$iEnd = strpos($this->_sCode, ')', $iBegin);
-		$aVars = explode(',', substr($this->_sCode, $iBegin, $iEnd - $iBegin));
-
-		// Get the static variables of the function via reflection
-		$aStaticVars = $this->_oReflection->getStaticVariables();
-
-		// Only keep the variables that appeared in both sets
-		$aUsedVars = [];
-		foreach ($aVars as $sVar) {
-			$sVar = trim($sVar, ' $&amp;');
-			$aUsedVars[$sVar] = $aStaticVars[$sVar];
-		}
-		return $aUsedVars;
-
+		return $this->_oReflection->invokeArgs(func_get_args());
 	}
 
 	/**
 	 * @return array
 	 */
 	public function __sleep() {
-		return ['_sCode', '_aUsedVariables'];
+		return ['_sSerialized'];
 	}
 
 	/**
-	 * @throws Exception
+	 * @return void
 	 */
 	public function __wakeup() {
-
-		# Recreate serialized function
-		extract($this->_aUsedVariables);
-		eval('$oFunction = '.$this->_sCode.';');
-
-		# Inject in handler
-		if (isset($oFunction) && $oFunction instanceOf Closure) {
-			$this->_oClosure = $oFunction;
-			$this->_oReflection = new ReflectionFunction($oFunction);
-		}
-		else {
-			throw new Exception;
-		}
-
+		$this->_oClosure = (new Serializer(new AstAnalyzer))->unserialize($this->_sSerialized);
+		$this->_oReflection = new ReflectionFunction($this->_oClosure);
 	}
 
 }
